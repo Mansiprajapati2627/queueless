b59076ca05587a22a3bd8fafce723affb2507077
Mansiprajapati2627 <mansiprajapati2627@gmail.com>
2026-04-01 11:24:55 +0530

    changes

--- backend/app/config/database.py
+++ backend/app/config/database.py
@@ -5,12 +5,15 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 # Get DB URL from Render
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set in environment variables")
+
 # Fix Render postgres URL
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-# Create engine
-engine = create_engine(DATABASE_URL)
+# Create engine (IMPORTANT SSL)
+engine = create_engine(DATABASE_URL, connect_args={"sslmode": "require"})
 
 # Session
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
