746f8e98627871a63a8817e0a16abd6b8ab1a05b
Mansiprajapati2627 <mansiprajapati2627@gmail.com>
2026-04-02 10:32:19 +0530

    maggie

--- backend/seed_db.py
+++ backend/seed_db.py
@@ -58,7 +58,7 @@ def random_price(min_val, max_val):
 # Image map (same as before)
 image_map = {
     "Snacks": [
-      '/assets/Snacks/Maggie.jpg',
+      'public/assets/Snacks/Maggie.jpg',
       '/assets/Snacks/Masalamaggie.jpg', 
       '/assets/Snacks/cheesemaggie.jpg',
       '/assets/Snacks/steamedmomos.jpg',
