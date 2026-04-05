0995e92ef7a025ecd769708fbdd0d5bafdbcb689
Mansiprajapati2627 <mansiprajapati2627@gmail.com>
2026-04-05 11:09:14 +0530

    tryyyyingggg

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,13 +8,18 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(title="QueueLess Backend")
 
 # CORS – allow all origins for debugging; replace with your frontend URL after testing
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://queueless-frontend-84br.onrender.com",
+        "http://localhost:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 app.include_router(auth_routes.router, prefix="/auth", tags=["Authentication"])
 app.include_router(user_routes.router, prefix="/users", tags=["Users"])
